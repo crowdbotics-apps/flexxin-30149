4abd92838343ab5aded7c2f54ec111e96db10818
Crowdbotics <team@crowdbotics.com>
2021-08-27 15:56:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "flexxin_30149Identifier",
+  url: "https://flexxin_30149Identifier.botics.co",
+  api: "https://flexxin_30149Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
